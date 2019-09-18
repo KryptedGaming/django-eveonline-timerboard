62e7c4bb9a37c30f6517f8f0ffb44e7d1902fee7
porowns <porowns@gmail.com>
2019-09-18 15:17:07 -0400

    Add custom loadable url_slug

--- django_eveonline_timerboard/apps.py
+++ django_eveonline_timerboard/apps.py
@@ -3,3 +3,5 @@ from django.apps import AppConfig
 
 class DjangoEveonlineTimerboardConfig(AppConfig):
     name = 'django_eveonline_timerboard'
+    verbose_name = 'EVE Online Timers'
+    url_slug = 'eveonline'
